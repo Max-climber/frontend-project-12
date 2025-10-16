86f97338056e8deada3b9cd6c0925ba599adec0b
Max-climber <max.izrailev04@mail.ru>
2025-10-17 01:35:45 +0300

    created app with vite and created Makefile

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	cd frontend && npm ci && npm run build
+
+start:
+	npx start-server -s ./frontend/dist
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+        // string shorthand
+        '/api/v1': 'http://localhost:5001',
+    }
+  }
+})
