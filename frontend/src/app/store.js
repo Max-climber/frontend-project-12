574472b6c4085e4423be5aabc329b983c048b067
Max-climber <max.izrailev04@mail.ru>
2025-10-27 23:54:55 +0300

    updated store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelReducer from '../features/channels/channelsSlice.js';
 import messagesReducer from '../features/messages/messagesSlice.js';
+import userReducer from '../features/users/userSlice.js';
 
 export default configureStore({
   reducer: {
     channels: channelReducer,    // Свойство channels будет внутри объекта общего состояния: state.channels
     messages: messagesReducer,   // Свойство messages будет внутри объекта общего состояния: state.messages
+    user: userReducer, // Свойство user будет внутри объекта общего состояния: state.user
   },
 })
