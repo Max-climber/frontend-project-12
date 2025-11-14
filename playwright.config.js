ab9d24a31ce3f091da72b0e10d11b676399c29e5
Max-climber <max.izrailev04@mail.ru>
2025-11-15 00:35:15 +0300

    fix: восстановлен оригинальный формат playwright.config.js и удалена папка tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,6 @@
-import { defineConfig, devices } from '@playwright/test'
+// @ts-check
+
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -16,13 +18,10 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // eslint-disable-next-line no-undef
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  // eslint-disable-next-line no-undef
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // eslint-disable-next-line no-undef
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
@@ -79,4 +78,4 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-})
+});
